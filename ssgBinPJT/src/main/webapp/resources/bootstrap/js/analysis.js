3becd34a2d0f9057ee81e0df6f685ca8943f622e
안태선/인사(임)/p90k6l <p90k6l@ssg.global>
2019-08-30 11:22:40 +0900

    merge

--- ssgBinPJT/src/main/webapp/resources/bootstrap/js/analysis.js
+++ ssgBinPJT/src/main/webapp/resources/bootstrap/js/analysis.js
@@ -1,7 +1,6 @@
 /**
  * 
  */
-
 var glp_ctx = document.getElementById('garbage-store-bar-1').getContext('2d');
 var glp_data = {
      datasets: [{
@@ -19,7 +18,8 @@ var glp_data = {
             borderWidth: 1
      }],
 
-     // These labels appear in the legend and in the tooltips when hovering different arcs
+     // These labels appear in the legend and in the tooltips when hovering
+		// different arcs
      labels: [
          'Can',
          'Bottle',
@@ -36,12 +36,11 @@ title	:{
 	text:'주간 누적 쓰레기량',
 	position: 'top'
 },
-
 layout:{
 	padding:{
-		left:70,
-		right:50,
-		bottom: 10
+		left : 70,
+		right : 50,
+		bottom : 10
 	}
 },
 
@@ -60,15 +59,16 @@ layout:{
  tooltips: {
     callbacks: {
       label: function(tooltipItem, data) {
-        //get the concerned dataset
+        // get the concerned dataset
         var dataset = data.datasets[tooltipItem.datasetIndex];
-        //calculate the total of this data set
+        // calculate the total of this data set
         var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
           return previousValue + currentValue;
         });
-        //get the current items value
+        // get the current items value
         var currentValue = dataset.data[tooltipItem.index];
-        //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+        // calculate the precentage based on the total and current item, also
+		// this does a rough rounding to give a whole number
         var percentage = Math.floor(((currentValue/total) * 100)+0.5);
 
         return data.labels[tooltipItem.index] + ": " + currentValue + " of " + total + "(" + percentage + "%)";
@@ -90,40 +90,39 @@ layout:{
             anchor : 'center',
             position : 'outside',
             align : 'center',
-//            offset: 1,
+// offset: 1,
             font : {
              size :10,
              style : 'bold'
-             //family, style, weight 가능
+             // family, style, weight 가능
             }
         }
     },
  onComplete: function () {
-    /* var ctx = this.chart.ctx;
-     ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-     ctx.textAlign = 'center';
-     ctx.textBaseline = 'bottom';
-     this.data.datasets.forEach(function (dataset) {
-       for (var i = 0; i < dataset.data.length; i++) {
-         var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-             total = dataset._meta[Object.keys(dataset._meta)[0]].total,
-             mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius)/2,
-             start_angle = model.startAngle,
-             end_angle = model.endAngle,
-             mid_angle = start_angle + (end_angle - start_angle)/2;
-         var x = mid_radius * Math.cos(mid_angle);
-         var y = mid_radius * Math.sin(mid_angle);
-         ctx.fillStyle = '#fff';
-         if (i == 3){ // Darker text color for lighter background
-           ctx.fillStyle = '#444';
-         }
-         var percent = String(Math.round(dataset.data[i]/total*100)) + "%";*/
+    /*
+	 * var ctx = this.chart.ctx; ctx.font =
+	 * Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily,
+	 * 'normal', Chart.defaults.global.defaultFontFamily); ctx.textAlign =
+	 * 'center'; ctx.textBaseline = 'bottom';
+	 * this.data.datasets.forEach(function (dataset) { for (var i = 0; i <
+	 * dataset.data.length; i++) { var model =
+	 * dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model, total =
+	 * dataset._meta[Object.keys(dataset._meta)[0]].total, mid_radius =
+	 * model.innerRadius + (model.outerRadius - model.innerRadius)/2,
+	 * start_angle = model.startAngle, end_angle = model.endAngle, mid_angle =
+	 * start_angle + (end_angle - start_angle)/2; var x = mid_radius *
+	 * Math.cos(mid_angle); var y = mid_radius * Math.sin(mid_angle);
+	 * ctx.fillStyle = '#fff'; if (i == 3){ // Darker text color for lighter
+	 * background ctx.fillStyle = '#444'; } var percent =
+	 * String(Math.round(dataset.data[i]/total*100)) + "%";
+	 */
         // ctx.fillText(dataset.data[i]+"%", model.x + x, model.y + y);
-         // Display percent in another line, line break doesn't work for fillText
-         //ctx.fillText(percent, model.x + x, model.y + y + 15);
+         // Display percent in another line, line break doesn't work for
+			// fillText
+         // ctx.fillText(percent, model.x + x, model.y + y + 15);
        }
-    // });   
- //}
+    // });
+ // }
 };
 var glp_chart = new Chart(glp_ctx, {
     type: 'pie',
@@ -135,79 +134,29 @@ var glp_chart = new Chart(glp_ctx, {
  * 매장별 차트 #1
  */
 /*
-var gsb_ctx_1 = document.getElementById('garbage-store-bar-1').getContext('2d');
-var gsb_data_1 = {
-     labels: ["Can", "Bottle", "Plastic", "Trash"],
-     datasets: [{
-       label: '# of Tomatoes',
-       data: [12, 19, 3, 5],
-       backgroundColor: [
-         'rgba(255, 99, 132, 0.2)',
-         'rgba(54, 162, 235, 0.2)',
-         'rgba(255, 206, 86, 0.2)',
-         'rgba(75, 192, 192, 0.2)',
-         'rgba(153, 102, 255, 0.2)',
-         'rgba(255, 159, 64, 0.2)',
-       ],
-       borderColor: [
-         'rgba(255,99,132,1)',
-         'rgba(54, 162, 235, 1)',
-         'rgba(255, 206, 86, 1)',
-         'rgba(75, 192, 192, 1)',
-         'rgba(153, 102, 255, 1)',
-         'rgba(255, 159, 64, 1)',
-       ],
-       borderWidth: 1
-     }]
-};
-var gsb_options_1 = {
-  legend: {
-   display: false
-  },
-     scales: {
-         xAxes: [{
-           ticks: {
-             //maxRotation: 90,
-             //minRotation: 80
-           }
-         }],
-         yAxes: [{
-           ticks: {
-             beginAtZero: true
-           },
-           scaleLabel: {
-                  display: true,
-                  labelString: "Count of Refresh"
-              }
-         }]
-       },
-       tooltips: {
-        callbacks: {
-       title: function() {},
-         label: function(tooltipItem, data) {
-           //get the concerned dataset
-           var dataset = data.datasets[tooltipItem.datasetIndex];
-           //calculate the total of this data set
-           var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-             return previousValue + currentValue;
-           });
-           //get the current items value
-           var currentValue = dataset.data[tooltipItem.index];
-           //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
-           var percentage = Math.floor(((currentValue/total) * 100)+0.5);
-           return data.labels[tooltipItem.index] + ": " + currentValue + " of " + total + "(" + percentage + "%)";
-         }
-       }
-      },
-      animation: {
-            onComplete: function() {
-              //rendering 후
-            }
-         }
-     };
-var gsb_chart_1 = new Chart(gsb_ctx_1, {
-    type: 'bar',
-    data: gsb_data_1,
-    options: gsb_options_1
-});
-*/
+ * var gsb_ctx_1 =
+ * document.getElementById('garbage-store-bar-1').getContext('2d'); var
+ * gsb_data_1 = { labels: ["Can", "Bottle", "Plastic", "Trash"], datasets: [{
+ * label: '# of Tomatoes', data: [12, 19, 3, 5], backgroundColor: [ 'rgba(255,
+ * 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+ * 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64,
+ * 0.2)', ], borderColor: [ 'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)',
+ * 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)',
+ * 'rgba(255, 159, 64, 1)', ], borderWidth: 1 }] }; var gsb_options_1 = {
+ * legend: { display: false }, scales: { xAxes: [{ ticks: { //maxRotation: 90,
+ * //minRotation: 80 } }], yAxes: [{ ticks: { beginAtZero: true }, scaleLabel: {
+ * display: true, labelString: "Count of Refresh" } }] }, tooltips: { callbacks: {
+ * title: function() {}, label: function(tooltipItem, data) { //get the
+ * concerned dataset var dataset = data.datasets[tooltipItem.datasetIndex];
+ * //calculate the total of this data set var total =
+ * dataset.data.reduce(function(previousValue, currentValue, currentIndex,
+ * array) { return previousValue + currentValue; }); //get the current items
+ * value var currentValue = dataset.data[tooltipItem.index]; //calculate the
+ * precentage based on the total and current item, also this does a rough
+ * rounding to give a whole number var percentage =
+ * Math.floor(((currentValue/total) * 100)+0.5); return
+ * data.labels[tooltipItem.index] + ": " + currentValue + " of " + total + "(" +
+ * percentage + "%)"; } } }, animation: { onComplete: function() { //rendering 후 } } };
+ * var gsb_chart_1 = new Chart(gsb_ctx_1, { type: 'bar', data: gsb_data_1,
+ * options: gsb_options_1 });
+ */
